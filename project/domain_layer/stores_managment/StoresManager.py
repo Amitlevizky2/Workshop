ed40a1c6f7a50c0b79a065a66ab582b1884841ce
CiTRuS93 <hadarradah@gmail.com>
2020-04-01 12:45:54 +0300

    search stores tests #10

--- project/domain_layer/stores_managment/StoresManager.py
+++ project/domain_layer/stores_managment/StoresManager.py
@@ -6,8 +6,18 @@ class StoreManager:
     def __init__(self):
         self.stores = {int: Store}
         self.stores_idx = 0
-    
+
     def search(self, search_term: str = "", categories: [str] = None, key_words: [str] = None) -> {Store: [Product]}:
+        """
+
+        Args:
+            search_term: part of the wanted product name
+            categories: categories to search in
+            key_words:
+
+        Returns:dist {Store:list of products in store}
+
+        """
         search_result = {}
         for store_id in self.stores.keys():
             search_in_store = self.stores.get(store_id).search(search_term, categories, key_words)
