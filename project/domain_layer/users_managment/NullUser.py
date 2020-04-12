2ac31c144836ed4c94a01dc32ab45981c449e996
lielleravid <lielle@bgu.post.ac.il>
2020-04-12 18:40:18 +0300

    guest registration including RegisteredUser.py #7

---
+++ project/domain_layer/users_managment/NullUser.py
@@ -0,0 +1,28 @@
+from project.domain_layer.users_managment.RegisteredUser import RegisteredUser
+
+
+class NullUser(RegisteredUser):
+
+    def __init__(self):
+        super(NullUser, self).__init__("null")
+
+    def view_purchase_history(self):
+        return None
+
+    def loggout(self):
+        return False
+
+    def login(self):
+        return False
+
+    def view_cart(self):
+        return None
+
+    def remove_product(self, store, product, quantity):
+        return False
+
+    def add_product(self, store, product, quantity):
+        return False
+
+    def open_store(self):
+        return None
--- project/domain_layer/users_managment/UserManager.py
+++ project/domain_layer/users_managment/UserManager.py
@@ -1,4 +1,7 @@
 from project import service_layer
+from project.domain_layer.users_managment.NullUser import NullUser
+from project.domain_layer.users_managment.RegisteredUser import RegisteredUser
+from project.domain_layer.users_managment.User import User
 
 
 class UserManager:
@@ -11,3 +14,35 @@ class UserManager:
         self.guest_user_list = {}
         ##maybe dictionary {id, username}
         self.admins = []
+
+    def find_reg_user(self, username) -> RegisteredUser:
+        user = self.reg_user_list[username]
+        if user is None:
+            user = self.guest_user_list[username]
+            if user is None:
+                user = NullUser()
+        return user
+
+    def find_user(self, username) -> User:
+        user = self.reg_user_list[username]
+        if user is None:
+            user = NullUser()
+        return user
+
+    def register(self, user, username, password):
+        check = self.find_reg_user(username)
+        if isinstance(check, NullUser):
+            registered = RegisteredUser(username)
+            self.security.add_user(username, password)
+            registered.cart = user.cart
+            self.reg_user_list[username] = registered
+            return True
+        else:
+            return False
+
+        ##EVERYTIME SOMEONE OPENS THE SYSTEM A NEW USER IS CREATEDDDDDDDD
+
+    def add_guest_user(self):
+        user = User("guestUser" + str(self.incremental_id))
+        self.incremental_id += 1
+        self.guest_user_list[user.username] = user
