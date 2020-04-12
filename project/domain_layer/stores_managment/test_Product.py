1d4d8c3a39cc6a176234b325d1e3256eb23e74a1
CiTRuS93 <hadarradah@gmail.com>
2020-04-12 18:11:08 +0300

    test get price #13

---
+++ project/domain_layer/stores_managment/test_Product.py
@@ -0,0 +1,14 @@
+import unittest
+from datetime import datetime, timedelta
+
+from project.domain_layer.stores_managment.Product import Product, Discount
+
+
+class test_Product(unittest.TestCase):
+    def test_get_price(self):
+        date_str = '04-10-2020'
+        dt = timedelta(days=10)
+        date_object = datetime.strptime(date_str, '%m-%d-%Y')
+        self.product = Product("apple", 2, "food", None, 1)
+        self.product.discount.append(Discount(date_object, date_object + dt, 50))
+        self.assertEqual(1, self.product.get_price())
