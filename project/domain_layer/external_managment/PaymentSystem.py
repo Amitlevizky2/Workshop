c72766a258f2d044a49250a97085a5e243a809ab
CiTRuS93 <hadarradah@gmail.com>
2020-04-13 14:32:58 +0300

    external stub

--- project/domain_layer/external_managment/PaymentSystem.py
+++ project/domain_layer/external_managment/PaymentSystem.py
@@ -3,11 +3,11 @@ from project.domain_layer.users_managment.Cart import Cart
 
 
 class PaymentSystem:
-    def __init__(self,external_payment_system):
+    def __init__(self, external_payment_system):
         self.external_payment_system = external_payment_system
 
     def pay(self, user, cart: Cart) -> [Purchase]:
-        if self.external_payment_system.pay(user,cart):
+        if self.external_payment_system.pay(user, cart):
             res = []
             for store in cart.baskets.keys():
                 res.append(Purchase(cart.baskets.get(store).products, user, store))
--- project/service_layer/PurchaseManager.py
+++ project/service_layer/PurchaseManager.py
@@ -4,16 +4,20 @@ from project.service_layer.StoresManagerInterface import StoresManagerInterface
 from project.service_layer.UsersManagerInterface import UsersManagerInterface
 
 
+class ExternalPaymentStub:
+    def pay(self, cart):
+        return True
+
+
 class PurchaseManager:
-    def __init__(self, user_manager:UsersManagerInterface , store_manager: StoresManagerInterface):
+    def __init__(self, user_manager: UsersManagerInterface, store_manager: StoresManagerInterface):
         self.user_manager = user_manager
         self.store_manager = store_manager
-        self.payment_system = PaymentSystem()
+        self.payment_system = PaymentSystem(ExternalPaymentStub())
         self.shipment_system = ShipmentSystem()
 
     def buy(self, user):
         for purchase in self.payment_system.pay(user, self.user_manager.view_cart(user)):
-
             self.store_manager.buy(self.user_manager.view_cart(user))
             self.store_manager.add_purchase_to_store(purchase.store_id, purchase)
             self.user_manager.add_purchase(user, purchase)
