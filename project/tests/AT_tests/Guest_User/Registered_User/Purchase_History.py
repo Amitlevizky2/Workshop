339c73c05feed3c718a00ec830ef44bf9fff3862
jenika <kevgeny@post.bgu.ac.il>
2020-04-16 13:37:54 +0300

    Registered_Purchase_History #16

---
+++ project/tests/AT_tests/Guest_User/Registered_User/Purchase_History.py
@@ -0,0 +1,21 @@
+import unittest
+from Driver import Driver
+from AT.test_env.Guest_user import ATsetUP
+class purchase_History(unittest.TestCase):
+    def setUp(self):
+        self.service = Driver.make_bridge()
+        ATsetUP.setUp(self.service)
+
+        self.service.register("user","pass")
+        self.service.login("user", "pass")
+        res = self.service.searchProduct("Banana")
+        first_store_id = list(res)[0]
+        self.service.add_product(first_store_id,res.get(first_store_id),2)
+        self.service.buy()
+    def test_purchase_happy(self):
+        result = self.service.get_purchase_history()
+        self.assertEqual(result.products[0].name, "Banana")
+
+
+if __name__ == '__main__':
+    unittest.main()
