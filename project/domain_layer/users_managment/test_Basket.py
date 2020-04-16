504822f0c3278ff678e5639f209ab230c7de9334
Noa Ben-Ami <benamn@post.bgu.ac.il>
2020-04-16 13:36:03 +0300

    implemented user related test classes

--- project/domain_layer/users_managment/test_Basket.py
+++ project/domain_layer/users_managment/test_Basket.py
@@ -1,12 +1,53 @@
+from datetime import timedelta, datetime
 from unittest import TestCase
+
+from project.domain_layer.stores_managment.Product import Product, Discount
+from project.domain_layer.users_managment.Basket import Basket
 
 
 class TestBasket(TestCase):
+    def setUp(self) -> None:
+        # 1234 for store id
+        self.basket = Basket(1234)
+        date_str = '04-10-2020'
+        dt = timedelta(days=10)
+        date_object = datetime.strptime(date_str, '%m-%d-%Y')
+        self.product_orange = Product("orange", 2, "food", None, 100)
+        self.product_orange.discount.append(Discount(date_object, date_object + dt, 50))
+        self.product_apple = Product("apple", 2, "food", None, 100)
+        self.product_apple.discount.append(Discount(date_object, date_object + dt, 50))
+        self.product_petunia = Product("petunia", 5, "food", None, 100)
+        self.product_petunia.discount.append(Discount(date_object, date_object + dt, 50))
+        self.product_begonia = Product("begonia", 15, "food", None, 100)
+        self.product_begonia.discount.append(Discount(date_object, date_object + dt, 50))
+
     def test_get_total(self):
-        self.fail()
+        self.basket.products.clear()
+        self.basket.products["orange"] = (self.product_orange, 3)
+        total = self.basket.get_total()
+        self.assertEqual(total, 3)
 
     def test_add_product(self):
-        self.fail()
+        # reset products in basket
+        self.basket.products.clear()
+        self.basket.products["orange"] = (self.product_orange, 3)
+        # products test dict contains 5 oranges and 2 apples
+        test_products = {}
+        pass
+        test_products["orange"] = (self.product_orange, 5)
+        test_products["apple"] = (self.product_apple, 2)
+        # adding 2 oranges and 2 apples to basket
+        self.basket.add_product(self.product_orange, 2)
+        self.basket.add_product(self.product_apple, 2)
+        # test equality of dicts
+        self.assertDictEqual(test_products, self.basket.products)
 
     def test_remove_product(self):
-        self.fail()
+        # reset products in basket
+        self.basket.products.clear()
+        self.basket.products["orange"] = (self.product_orange, 3)
+        # remove all products from basket
+        self.basket.remove_product(self.product_orange, 3)
+        self.assertDictEqual(self.basket.products, {})
+
+
--- project/domain_layer/users_managment/test_Cart.py
+++ project/domain_layer/users_managment/test_Cart.py
@@ -1,24 +1,98 @@
+from datetime import timedelta, datetime
 from unittest import TestCase
+
+from project.domain_layer.stores_managment.Product import Product, Discount
+from project.domain_layer.users_managment.Basket import Basket
+from project.domain_layer.users_managment.Cart import Cart
 
 
 class TestCart(TestCase):
+    def setUp(self) -> None:
+        # baskets = {store_id, basket}
+        self.cart = Cart()
+        date_str = '04-10-2020'
+        dt = timedelta(days=10)
+        date_object = datetime.strptime(date_str, '%m-%d-%Y')
+        self.product_orange = Product("orange", 2, "food", None, 100)
+        self.product_orange.discount.append(Discount(date_object, date_object + dt, 50))
+        self.product_apple = Product("apple", 2, "food", None, 100)
+        self.product_apple.discount.append(Discount(date_object, date_object + dt, 50))
+        self.product_petunia = Product("petunia", 5, "food", None, 100)
+        self.product_petunia.discount.append(Discount(date_object, date_object + dt, 50))
+        self.product_begonia = Product("begonia", 15, "food", None, 100)
+        self.product_begonia.discount.append(Discount(date_object, date_object + dt, 50))
+
     def test_add_basket(self):
-        self.fail()
+        # reset baskets & add new basket
+        self.cart.baskets.clear()
+        self.cart.add_basket(1234)
+
+        self.assertTrue(1234 in self.cart.baskets.keys())
 
     def test_remove_basket(self):
-        self.fail()
+        self.cart.baskets.clear()
+
+        basket1 = Basket(1234)
+        basket2 = Basket(5678)
+        self.cart.baskets[1234] = basket1
+        self.cart.baskets[5678] = basket2
+
+        self.cart.remove_basket(1234)
+
+        self.assertTrue(1234 not in self.cart.baskets.keys())
+        self.assertTrue(5678 in self.cart.baskets.keys())
 
     def test_get_basket(self):
-        self.fail()
+        self.cart.baskets.clear()
+        basket1 = Basket(1234)
+        self.cart.baskets[1234] = basket1
+        basket2 = self.cart.get_basket(2222)
+        basket1_tag = self.cart.get_basket(1234)
+
+        self.assertIsNone(basket2)
+        self.assertEqual(basket1, basket1_tag)
 
     def test_get_total(self):
-        self.fail()
+        self.cart.baskets.clear()
+        # total price = 10
+        basket1 = Basket(1234)
+        basket1.products["orange"] = (self.product_orange, 3)
+        basket1.products["apple"] = (self.product_apple, 2)
+
+        # total price = 125
+        basket2 = Basket(5678)
+        basket2.products["petunia"] = (self.product_petunia, 10)
+        basket2.products["begonia"] = (self.product_begonia, 5)
+
+        self.cart.baskets[1234] = basket1
+        self.cart.baskets[5678] = basket2
+
+        total_price = self.cart.get_total()
+        self.assertEqual(total_price, 67.5)
 
     def test_view(self):
-        self.fail()
+        pass
 
     def test_remove_product(self):
-        self.fail()
+        self.cart.baskets.clear()
+
+        basket1 = Basket(1234)
+        basket1.products["orange"] = (self.product_orange, 3)
+        basket1.products["apple"] = (self.product_apple, 2)
+
+        basket2 = Basket(5678)
+        basket2.products["petunia"] = (self.product_petunia, 10)
+
+        self.cart.baskets[1234] = basket1
+        self.cart.baskets[5678] = basket2
+
+        self.cart.remove_product(1234, self.product_orange, 3)
+        self.cart.remove_product(1234, self.product_apple, 2)
+        self.cart.remove_product(5678, self.product_petunia, 5)
+
+        self.assertTrue(1234 not in self.cart.baskets.keys())
+        self.assertEqual(basket2.products["petunia"][1], 5)
+        self.assertFalse(self.cart.remove_product(5678, self.product_begonia, 5))
 
     def test_add_product(self):
-        self.fail()
+        pass
