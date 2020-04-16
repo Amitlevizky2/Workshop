a842c5dbf91e35ebcec25d44b97ec4a9d780b8e5
jenika <kevgeny@post.bgu.ac.il>
2020-04-16 17:00:43 +0300

    Guest_User_Registration #7

---
+++ project/tests/AT_tests/Guest_User/Guest_registration.py
@@ -0,0 +1,27 @@
+import unittest
+from project.tests.AT_tests.test_env.Driver import Driver
+
+
+class Register(unittest.TestCase):
+
+    def setUp(self):
+        self.service = Driver.make_bridge()
+
+        res2 = self.service.register("username", "password")
+
+    def test_happy_registration(self):
+        res1 = self.service.register("username", "password")
+        self.assertTrue(res1)
+
+    def test_sad_registration(self):
+        res1 = self.service.register("username", "password")
+        res2 = self.service.register("username", "password")
+        self.assertEqual(res1, res2)
+
+    def test_bad_registration(self):
+        result1 = self.service.register("", "password")
+        self.assertIsNotNone(result1)
+
+
+if __name__ == '__main__':
+    unittest.main()
