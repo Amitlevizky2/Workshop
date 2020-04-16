e2da644b3ddc2cea44de54a08c7cca2e86de7141
jenika <kevgeny@post.bgu.ac.il>
2020-04-16 20:02:05 +0300

    proxy changes

--- project/tests/AT_tests/test_env/Proxy.py
+++ project/tests/AT_tests/test_env/Proxy.py
@@ -116,8 +116,13 @@ class Proxy:
         if self.real != None:
             return self.real.add_product(store_id, product, amount)
         else:
-
-            return True
+            purchase_type = type('Purchase', (object,), {})
+            product_type = type('Product', (object,), {})
+            p = product_type()
+            p.name = "Banana"
+            pur = purchase_type()
+            pur.products = [p]
+            return pur
 
     def buy(self):
         if self.real != None:
