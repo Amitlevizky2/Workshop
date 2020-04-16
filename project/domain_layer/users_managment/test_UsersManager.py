878209271e0f4220180f6ced0fc85d75bdb55609
Noa Ben-Ami <benamn@post.bgu.ac.il>
2020-04-16 19:18:56 +0300

    #24

--- project/domain_layer/users_managment/test_UsersManager.py
+++ project/domain_layer/users_managment/test_UsersManager.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 from project.domain_layer.external_managment.Purchase import Purchase
 from project.domain_layer.stores_managment.Product import Product
+from project.domain_layer.users_managment.NullUser import NullUser
 from project.domain_layer.users_managment.RegisteredUser import RegisteredUser
 from project.domain_layer.users_managment.User import User
 from project.domain_layer.users_managment.UsersManager import UsersManager
@@ -10,18 +11,24 @@ from project.domain_layer.users_managment.UsersManager import UsersManager
 class TestUserManager(TestCase):
     def setUp(self) -> None:
         self.users_mng = UsersManager()
-        self.reg_user1 = RegisteredUser("reg_user1")
 
     def test_find_reg_user(self):
-        self.users_mng.reg_user_list.pop("reg_user1")
-        if "reg_user1" not in self.users_mng.reg_user_list.keys():
-            self.users_mng.reg_user_list["reg_user1"] = self.reg_user1
-
+        reg_user1 = RegisteredUser("reg_user1")
+        self.users_mng.reg_user_list["reg_user1"] = reg_user1
         reg_user_test = self.users_mng.find_reg_user("reg_user1")
-        self.assertEqual(reg_user_test, self.reg_user1)
+        self.assertEqual(reg_user_test, reg_user1)
+        self.users_mng.reg_user_list.pop("reg_user1")
 
     def test_find_user(self):
-        self.fail()
+        user = User("guestUser111")
+        self.users_mng.guest_user_list[user.username] = user
+        reg_user1 = RegisteredUser("reg_user1")
+        self.users_mng.reg_user_list["reg_user1"] = reg_user1
+        self.assertEqual(user, self.users_mng.find_user("guestUser111"))
+        self.assertEqual(reg_user1, self.users_mng.find_user("reg_user1"))
+
+        self.assertTrue(isinstance(self.users_mng.find_user("not_real_user"), NullUser))
+        self.users_mng.reg_user_list.pop("reg_user1")
 
     def test_register(self):
         user = User("guestUser700")
@@ -77,7 +84,22 @@ class TestUserManager(TestCase):
 
     def test_view_purchases(self):
         # #24
-        self.fail()
+        reg_lielle = RegisteredUser("lielle")
+        self.users_mng.reg_user_list["lielle"] = reg_lielle
+        product_orange = Product("orange", 2, "food", None, 100)
+        purchase = Purchase([product_orange], "lielle", 1234, 1)
+        reg_lielle.purchase_history.append(purchase)
+        purch = [purchase]
+
+        x = self.users_mng.view_purchases("lielle")
+        self.assertListEqual(x, purch)
+
+        user = User("guestUser1212")
+        self.users_mng.guest_user_list[user.username] = user
+        user.purchase_history.append(purchase)
+        self.assertListEqual(self.users_mng.view_purchases("guestUser1212"), purch)
+
+        self.users_mng.reg_user_list.pop("lielle")
 
     def test_add_product(self):
         pass
@@ -107,7 +129,8 @@ class TestUserManager(TestCase):
         self.users_mng.reg_user_list.pop("lielle")
 
     def test_is_admin(self):
-        self.fail()
+        self.assertTrue(self.users_mng.is_admin("admin"))
+        self.assertFalse(self.users_mng.is_admin("not_admin"))
 
     def test_add_managed_store(self):
         self.fail()
