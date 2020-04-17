441537c88598a3b1d46f87055df07426852887cc
jenika <kevgeny@post.bgu.ac.il>
2020-04-17 18:40:07 +0300

    AT_admin_view #24

--- project/tests/AT_tests/System_manager/test_admin_view_purchase_history.py
+++ project/tests/AT_tests/System_manager/test_admin_view_purchase_history.py
@@ -2,13 +2,14 @@ import unittest
 from project.tests.AT_tests.test_env.Driver import Driver
 from project.tests.AT_tests import ATsetUP
 
+
 class admin_view_purchase_history(unittest.TestCase):
 
     def setUp(self):
         self.service = Driver.make_bridge()
         self.store_id = ATsetUP.setup(self.service)
-        self.service.register("admin","1234")
-        self.service.login("admin","1234")
+        self.service.register("admin", "1234")
+        self.service.login("admin", "1234")
         self.service.logout()
 
     def test_admin_view_happy(self):
@@ -16,23 +17,25 @@ class admin_view_purchase_history(unittest.TestCase):
         if self.service.login("user", "pass"):
             res = self.service.searchProduct("Banana")
             first_store_id = list(res)[0]
-            self.service.add_product(first_store_id, res.get(first_store_id), 2)
-            self.service.buy()
-            if self.service.admin:
-                    result = self.service.get_purchase_history()
-                    history = self.service.view_store_history(self.store_id)
-                    self.assertEqual(result.products[0].name, "Banana")
-                    self.assertIsNotNone(history)
-                    self.assertEqual("Apple", history.products[0].product_name)
-
-    def test_admin_view_sad(self):
-        if self.service.admin:
-            res = self.service.searchProduct("apple")
-            first_store_id = list(res)[0]
-            self.service.add_product(first_store_id, res.get(first_store_id), 2)
+            self.service.add_product(first_store_id, res.get(first_store_id)[0], 2)
             self.service.buy()
             result = self.service.get_purchase_history()
-            self.assertNotEqual(result.products[0].name, "apple")
+            self.service.logout()
+            self.service.login("admin", "1234")
+            history = self.service.view_store_history(self.store_id)
+            self.assertIn( "Banana", result[0].products.keys())
+            self.assertIsNotNone(history)
+            self.assertIn("Banana", history[0].products.keys())
+
+    def test_admin_view_sad(self):
+        res = self.service.searchProduct("Apple")
+        first_store_id = list(res)[0]
+        self.service.add_product(first_store_id, res.get(first_store_id)[0], 2)
+        self.service.buy()
+        result = self.service.get_purchase_history()
+        self.service.logout()
+        self.service.login("admin", "1234")
+        self.assertNotIn("apple", result[0].products.keys())
 
     def test_admin_view_bad(self):
         history = None
@@ -40,11 +43,6 @@ class admin_view_purchase_history(unittest.TestCase):
         if self.service.admin:
             history = self.service.view_store_history(self.store_id)
         self.assertIsNone(history)
-
-
-
-
-
 
 
 if __name__ == '__main__':
