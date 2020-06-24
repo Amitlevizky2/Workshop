bc50180c8a364e8264160004274639cf02c58f2e
jenika <kevgeny@post.bgu.ac.il>
2020-06-24 17:48:23 +0300

    ESAPI

--- project/service_layer/ExternalserviseAPI.py
+++ project/service_layer/ExternalserviseAPI.py
@@ -14,22 +14,31 @@ class ExternalServiceAPI:
             ,"month": month, 'year':year, 'holder': holder
             ,'ccv': ccv, 'id': id}
         rpay = requests.post("https://cs-bgu-wsep.herokuapp.com/",data = paydtls)
+        if rpay == -1:
+            return "The transaction was declined"
         return rpay
 
     def cancelpay(self,r):
         canceldtl = {'action_type': 'cancel_pay','transaction_id':r}
         rcancle = requests.post(self.url,data= canceldtl)
-        return rcancle
+        if rcancle == -1:
+            return "The transaction was declined"
+        elif rcancle ==1:
+            return rcancle
 
     def supply(self,name,address,city,country,zip):
         supplydtls = {'action_type': 'supply', 'name': name
                       ,'address':address,'city':city
                       ,'country':country,'zip':zip}
         rsupply = requests.post(self.url,data= supplydtls)
+        if rsupply == -1:
+            return "The shipment was declined"
         return rsupply
 
     def cancelsupply(self,rsupply):
         cancelsupplydtl = {'action_type': 'cancel_supply','transaction_id':rsupply}
         rcanclesupply = requests.post(self.url,data= cancelsupplydtl)
-
+        if rcanclesupply == -1:
+            return "The shipment was declined"
+        return rcanclesupply
 
