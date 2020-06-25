2260a62fd85ce05e7673eef52ba364b04250eda1
Amitlevizky2 <levamit@post.bgu.ac.il>
2020-06-25 12:07:23 +0300

    purchase orm

--- project/tests/AT_tests/store_manager/test_store_manager.py
+++ project/tests/AT_tests/store_manager/test_store_manager.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 
+from project.data_access_layer import Base, engine
 from project.tests.AT_tests.store_owner.test_add_store_permiision_to_manager import AddStorePermission
 from project.tests.AT_tests.test_env.Driver import Driver
 from project.tests.AT_tests import ATsetUP
@@ -36,5 +37,40 @@ class StoreManagerTest(unittest.TestCase):
     def test_manager_action_bad(self):
         self.service.logout()
         self.assertTrue(self.service.add_product_to_Store(self.store_id, *ATsetUP.products[0])['error'])
+
+
+    def tearDown(self) -> None:
+        self.drop_table('stores')
+        self.drop_table('baskets')
+        self.drop_table('CompositeDiscounts')
+        self.drop_table('CompositePolicies')
+        self.drop_table('conditionalproductdiscounts')
+        self.drop_table('conditionalstorediscounts')
+        self.drop_table('discounts')
+        self.drop_table('to_apply_composite')
+        self.drop_table('managers')
+        self.drop_table('managerpermissions')
+        self.drop_table('owners')
+        self.drop_table('Policy_in_composite')
+        self.drop_table('policies')
+        self.drop_table('predicates')
+        self.drop_table('products')
+        self.drop_table('productspolicies')
+        self.drop_table('productsinbaskets')
+        self.drop_table('Discount_products')
+        self.drop_table('Policy_products')
+        self.drop_table('productsinpurcases')
+        self.drop_table('purchases')
+        self.drop_table('regusers')
+        self.drop_table('stores')
+        self.drop_table('storepolicies')
+        self.drop_table('passwords')
+        self.drop_table('notifications')
+        self.drop_table('visibleProductDiscounts')
+        # self.drop_table('stores')
+
+    def drop_table(self, table_name: str):
+        if table_name in Base.metadata.tables:
+            Base.metadata.drop_all(engine, [Base.metadata.tables[table_name]])
 if __name__ == '__main__':
     unittest.main()
